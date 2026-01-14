1d740896e2ae9dbcebb81599df2af9e1a70521f5
IlyasBudi <wjati145@gmail.com>
2026-01-14 19:21:25 +0700

    fix: Reduce the number of popular and reward products displayed to 5

--- app/Http/Controllers/Customer/PagesController.php
+++ app/Http/Controllers/Customer/PagesController.php
@@ -25,7 +25,7 @@ class PagesController extends Controller
             ->where('is_reward', false)
             ->with(['category', 'images', 'variations'])
             ->orderBy('created_at', 'desc')
-            ->take(10)
+            ->take(5)
             ->get();
 
         // Ambil produk reward (produk yang aktif dan adalah reward)
@@ -33,7 +33,7 @@ class PagesController extends Controller
             ->where('is_reward', true)
             ->with(['category', 'images', 'variations'])
             ->orderBy('created_at', 'desc')
-            ->take(10)
+            ->take(5)
             ->get();
 
         // Ambil banners jika ada
