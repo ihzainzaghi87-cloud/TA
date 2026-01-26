bb74901b5d4d28e00061f24a94fdb988eec62654
IlyasBudi <wjati145@gmail.com>
2026-01-27 00:34:12 +0700

    fix: replace SVG icon with image for Shopee link in footer

--- resources/views/components/customer/footer.blade.php
+++ resources/views/components/customer/footer.blade.php
@@ -94,10 +94,8 @@
                     </svg>
                 </a>
 
-                <a href="#" class="bg-gray-100 hover:bg-black rounded-full w-8 h-8 flex items-center justify-center transition-colors group">
-                    <svg class="w-4 h-4 text-gray-600 group-hover:text-white transition-colors" fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z"/>
-                    </svg>
+                <a href="https://shopee.co.id/shop/1639591837" class="bg-gray-100 hover:bg-black rounded-full w-8 h-8 flex items-center justify-center transition-colors group">
+                    <img src="/ui/marketplace/shopee.png" alt="Shopee" class="w-5 h-5 object-contain group-hover:brightness-200 transition-colors" />
                 </a>
 
                 <a href="#" class="bg-gray-100 hover:bg-black rounded-full w-8 h-8 flex items-center justify-center transition-colors group">
